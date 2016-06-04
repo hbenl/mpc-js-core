33c64f6cfd2cb1cbcc4e8702a549da2f17e8fbcc
Holger Benl <hbenl@evandor.de>
2016-06-04 16:29:11 +0200

    add missing command

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -1,16 +1,21 @@
 import { MPDProtocol } from '../protocol';
-import { Status, Statistics } from '../objects/index';
+import { PlaylistItem, Status, Statistics } from '../objects/index';
 
 export class StatusCommands {
 
 	constructor(private protocol: MPDProtocol) {}
 
-	getStatus(): Promise<Status> {
+	currentSong(): Promise<PlaylistItem> {
+		return this.protocol.sendCommand('currentsong').then(
+			(lines) => this.protocol.parse(lines, ['file'], (valueMap) => new PlaylistItem(valueMap))[0]);
+	}
+
+	status(): Promise<Status> {
 		return this.protocol.sendCommand('status').then(
 			(msg) => this.protocol.parse<Status>(msg, [], (valueMap) => new Status(valueMap))[0]);
 	}
 
-	getStatistics(): Promise<Statistics> {
+	statistics(): Promise<Statistics> {
 		return this.protocol.sendCommand('stats').then(
 			(msg) => this.protocol.parse<Statistics>(msg, [], (valueMap) => new Statistics(valueMap))[0]);
 	}
